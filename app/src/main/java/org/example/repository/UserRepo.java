df403199f91e36795144eef992e0ae4d1ce5cd0a
SaqibCod <saqibbca2021@gmail.com>
2025-04-10 08:33:40 +0530

    RefreshTokenService:

--- app/src/main/java/org/example/repository/UserRepo.java
+++ app/src/main/java/org/example/repository/UserRepo.java
@@ -1,4 +1,10 @@
 package org.example.repository;
 
-public interface UserRepo {
+import org.example.entities.UserInfo;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends CrudRepository<UserInfo, String> {
+    public UserInfo findByUsername(String username);
 }
