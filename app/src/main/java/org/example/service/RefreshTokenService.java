f85fcf5a5bd35a130c93f1c67fe0ce492953a7dd
SaqibCod <saqibbca2021@gmail.com>
2025-04-19 12:28:10 +0530

    Sign up User

--- app/src/main/java/org/example/service/RefreshTokenService.java
+++ app/src/main/java/org/example/service/RefreshTokenService.java
@@ -42,4 +42,5 @@ public class RefreshTokenService {
         return token;
     }
 
+
 }
--- app/src/main/java/org/example/service/UserDetailService.java
+++ app/src/main/java/org/example/service/UserDetailService.java
@@ -2,6 +2,7 @@ package org.example.service;
 
 import lombok.Data;
 import org.example.entities.UserInfo;
+import org.example.model.UserInfoDto;
 import org.example.repository.UserRepo;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 @Data
@@ -32,5 +36,20 @@ public class UserDetailService implements UserDetailsService {
         }
 //        log.info("User Authenticated Successfully..!!!");
         return new CustomUserDetail(user);
+    }
+
+    public UserInfo checkIfUserAlreadyExist(UserInfoDto userInfoDto) {
+        return userRepository.findByUsername(userInfoDto.getUsername());
+    }
+
+    public Boolean signupUser(UserInfoDto userInfoDto){
+        userInfoDto.setPassword(passwordEncoder.encode(userInfoDto.getPassword()));
+        if(Objects.nonNull(checkIfUserAlreadyExist(userInfoDto))){
+            return false;
+        }
+        String userId = UUID.randomUUID().toString();
+        userRepository.save(new UserInfo(userId, userInfoDto.getUsername(),
+                userInfoDto.getPassword(), new HashSet<>()));
+        return true;
     }
 }
